5f2dec31befe5bd262305e76936371b26702710f
adamchok <adam.c11304@gmail.com>
2025-05-18 15:22:10 +0800

    Update MytelmedBackendApplication.java

--- mytelmed-backend/src/main/java/com/mytelmed/MytelmedBackendApplication.java
+++ mytelmed-backend/src/main/java/com/mytelmed/MytelmedBackendApplication.java
@@ -2,13 +2,13 @@ package com.mytelmed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class MytelmedBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MytelmedBackendApplication.class, args);
     }
-
 }
